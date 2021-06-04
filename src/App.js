d32a3eecd482125bb5a9fea977ed12352a4978ac
renan_bessa07 <renan.bessa@idinheiro.com.br>
2021-06-04 11:16:12 -0300

    setado os valores no estado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { CurrencyRow } from './components/CurrencyRow';
+import axios from 'axios';
 
 function App() {
+  const [rates, setRates] = useState([]);
+
+  const baseURL = 'http://api.exchangeratesapi.io/';
+  const apiKey = '5b5f6aab54b9f47c0ef54fad8d66be41';
+
+  const getAllRates = () => {
+    axios
+      .get(`${baseURL}v1/latest?access_key=${apiKey}`)
+      .then((response) => {
+        const allRates = response.data;
+        setRates(allRates);
+      })
+      .catch((error) => console.error(`Error: ${error}`));
+  };
+
+  useEffect(() => {
+    getAllRates();
+  }, []);
+
+  console.log(rates);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div className="equals">=</div>
+      <CurrencyRow />
     </div>
   );
 }
---
+++ src/components/CurrencyRow/index.jsx
@@ -0,0 +1,10 @@
+export const CurrencyRow = () => {
+  return (
+    <div>
+      <input type="number" className="input" />
+      <select>
+        <option value="Hi">Hi</option>
+      </select>
+    </div>
+  );
+};
