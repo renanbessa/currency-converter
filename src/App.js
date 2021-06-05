63787417dd531fdbb1709295254349c4918923a1
renan_bessa07 <renan.bessa@idinheiro.com.br>
2021-06-05 11:10:58 -0300

    Criado estrutura inicial

--- src/App.js
+++ src/App.js
@@ -4,16 +4,36 @@ import axios from 'axios';
 
 function App() {
   const [rates, setRates] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
 
   const baseURL = 'http://api.exchangeratesapi.io/';
   const apiKey = '5b5f6aab54b9f47c0ef54fad8d66be41';
+
+  let fromAmount, toAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
 
   const getAllRates = () => {
     axios
       .get(`${baseURL}v1/latest?access_key=${apiKey}`)
       .then((response) => {
-        const allRates = response.data;
-        setRates(allRates);
+        const allRates = response.data.rates;
+        const baseRate = response.data.base;
+        const firstCurrency = Object.keys(allRates)[0];
+
+        setRates([baseRate, ...Object.keys(allRates)]);
+        setFromCurrency(baseRate);
+        setToCurrency(firstCurrency);
+        setExchangeRate(allRates[firstCurrency]);
       })
       .catch((error) => console.error(`Error: ${error}`));
   };
@@ -22,14 +42,48 @@ function App() {
     getAllRates();
   }, []);
 
-  console.log(rates);
+  // useEffect(() => {
+  //   if (fromCurrency !== null && toCurrency !== null) {
+  //     axios
+  //       .get(`${baseURL}v1/latest?access_key=${apiKey}&base=${fromCurrency}&symbols=${toCurrency}`)
+  //       .then((response) => {
+  //         const allRates = response.data.rates;
+  //         const ExchangeRate = allRates[toCurrency];
+
+  //         setExchangeRate(ExchangeRate);
+  //       })
+  //       .catch((error) => console.error(`Error: ${error}`));
+  //   }
+  // }, [fromCurrency, toCurrency]);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
 
   return (
     <div className="App">
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow
+        rates={rates}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
       <div className="equals">=</div>
-      <CurrencyRow />
+      <CurrencyRow
+        rates={rates}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
     </div>
   );
 }
--- src/components/CurrencyRow/index.jsx
+++ src/components/CurrencyRow/index.jsx
@@ -1,10 +1,28 @@
-export const CurrencyRow = () => {
+import PropTypes from 'prop-types';
+
+const CurrencyRowPropTypes = {
+  rates: PropTypes.array.isRequired,
+  selectedCurrency: PropTypes.string,
+  onChangeCurrency: PropTypes.func.isRequired,
+  onChangeAmount: PropTypes.func.isRequired,
+  amount: PropTypes.number.isRequired,
+};
+
+export const CurrencyRow = (props) => {
+  const { rates, selectedCurrency, onChangeCurrency, onChangeAmount, amount } = props;
+
   return (
     <div>
-      <input type="number" className="input" />
-      <select>
-        <option value="Hi">Hi</option>
+      <input type="number" className="input" value={amount} onChange={onChangeAmount} />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {rates.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </div>
   );
 };
+
+CurrencyRow.propTypes = CurrencyRowPropTypes;
